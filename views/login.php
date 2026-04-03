61d7fa9d1d5f20452d2e9f532ef47bcd912b7cff
Juan <motadjuan@icloud.com>
2026-04-03 08:36:01 -0400

    Agregado una pantalla de log out, y redireccion al login

--- views/login.php
+++ views/login.php
@@ -1,6 +1,16 @@
-<form method="POST" action="../controllers/loginController.php">
-    <h2>Login</h2>
-    <input type="text" name="username" placeholder="Usuario" required>
-    <input type="password" name="password" placeholder="Contraseña" required>
-    <button type="submit">Entrar</button>
-</form>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<div class="container mt-5" style="max-width: 400px;">
+    <h3 class="text-center mb-4">Login</h3>
+
+    <form method="POST" action="controllers/loginController.php">
+        <div class="mb-3">
+            <input type="text" name="username" class="form-control" placeholder="Usuario">
+        </div>
+
+        <div class="mb-3">
+            <input type="password" name="password" class="form-control" placeholder="Contraseña">
+        </div>
+
+        <button name="login" class="btn btn-primary w-100">Ingresar</button>
+    </form>
+</div>
