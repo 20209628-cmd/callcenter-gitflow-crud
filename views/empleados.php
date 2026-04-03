0f73c8d010b991eccd08965748f91df18a9488b5
Juan <motadjuan@icloud.com>
2026-04-03 07:17:13 -0400

    arreglado el bug de llamadas.php y empleados.php

--- views/empleados.php
+++ views/empleados.php
@@ -1,3 +1,8 @@
+<?php
+include("../config/db.php");
+
+$result = $conn->query("SELECT * FROM empleados");
+?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <div class="container mt-5">
     <h2>Empleados</h2>
